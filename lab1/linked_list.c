8fa886dbff912c7d9d1856fb94974dc2bc48f78a
Victor Wang <11711201@mail.sustech.edu.cn>
2020-09-08 12:03:29 +0000

    lab1

--- lab1/linked_list.c
+++ lab1/linked_list.c
@@ -55,4 +55,156 @@ void linked_list_append(node *head, int val){
 }
 
 /* your implementation goes here */
+/* insert val at position index */
+void linked_list_insert(node *head, int val, int index){
 
+    if(head->count < index+1){
+        return;
+    } else if(head->count == index){
+        linked_list_append(head,val);
+        // head->count++;
+    } else {
+        int i = -1;
+        node * tmp = head;
+        do{
+            tmp = tmp->next;
+            ++i;
+        }while(i != index-1);
+        node *ins = (node*)malloc(sizeof(node));
+        ins->value = val;
+        ins->next = tmp->next;
+        tmp->next = ins;
+        head->count++; 
+    }
+    
+}
+
+/* delete node at position index */
+void linked_list_delete(node *head, int index){
+    if(head->count < index+1 || index < 0){
+        return;
+    } else {
+        int i = 0;
+        node * tmp = head;
+        
+        do{
+            i++;
+            tmp = tmp->next;
+        }while (i != index);
+        // tmp = tmp->next;
+        node * x  =tmp->next;
+        tmp->next = tmp->next->next;
+        free(x);
+        // if(tmp->next == NULL){
+        //     tmp=NULL;
+        // } else
+        // {
+        //     node * ttmp = tmp->next;
+        //     if(ttmp->next==NULL)
+        //         tmp->next = NULL;
+        //     else{
+        //         tmp->next = tmp->next->next;
+        //         // free(ttmp);
+        //         return;
+        //     }
+        //     // free(ttmp);
+        // }
+        
+        head->count--;
+    }
+    
+}
+
+/* remove the first occurence node of val */
+void linked_list_remove(node *head, int val){
+    node *tbr = head;
+    while (tbr->next != NULL && tbr->next->value != val)
+    {
+        tbr = tbr->next;
+    }
+    if(tbr->next != NULL){
+        node * tmp = tbr->next;
+        tbr->next = tbr->next->next;
+        free(tmp);
+        head->count--;
+    }
+    
+}
+
+/* remove all occurences of val */
+void linked_list_remove_all(node *head, int val){
+    node *tbr = head;
+    while(tbr->next != NULL){
+        while (tbr->next != NULL && tbr->next->value != val)
+        {
+            tbr = tbr->next;
+        }
+        if(tbr->next != NULL){
+            node * tmp = tbr->next;
+            tbr->next = tbr->next->next;
+            free(tmp);
+            head->count--;
+        }
+    }
+
+}
+
+/* get value at position index */
+int linked_list_get(node *head, int index){
+    if(head->count >= index+1){
+        int i = -1;
+        while(i != index){
+            head = head->next;
+            i++;
+        }
+        return head->value;
+    } else {
+        return -0x80000000;
+    }
+
+}
+
+/* search the first index of val */
+int linked_list_search(node *head, int val){
+    node *tbr = head;
+    int i = 0;
+    while (tbr->next != NULL && tbr->next->value != val)
+    {
+        tbr = tbr->next;
+        i++;
+    }
+    if(tbr->next != NULL){
+        return i;
+    }else{
+        return -1;
+    }
+}
+
+/* search all indexes of val */
+node *linked_list_search_all(node *head, int val){
+    node * h = linked_list_init();
+    // node * x = h;
+    node *tbr = head;
+    int i = 0;
+    while (1)
+    {
+        
+        while (tbr->next != NULL && tbr->next->value != val)
+        {
+            tbr = tbr->next;
+            i++;
+        }
+        if(tbr->next != NULL){
+            linked_list_append(h,i++);
+            tbr = tbr->next;
+        }else
+        {
+            // linked_list_append(h,i);
+            // printf("f");
+            break;
+        }
+        
+    }
+    return h;
+
+}
